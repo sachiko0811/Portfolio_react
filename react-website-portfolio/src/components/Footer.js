2951c721c21827a1684425a674c91990347a0050
sachiko1995 <sachikonovel@yahoo.co.jp>
2020-09-19 11:09:51 -0700

    json

---
+++ react-website-portfolio/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div>
+            <footer>
+        Copyright &copy; 2020 Sachiko Yokoyama, All rights Reserved.
+    </footer>
+        </div>
+    )
+}
+
+export default Footer
